3445ce89052c25d83528fc888f2a694af7565de0
knyliu <131148428+knyliu@users.noreply.github.com>
2024-01-01 16:26:30 +0800

    Add files via upload

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// 引入其他需要的組件
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// 定義您的路由
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/read",
+    element: <App />,
+  },
+  // 添加更多路由規則如需要
+  // 例如:
+  // {
+  //   path: "/another-route",
+  //   element: <AnotherComponent />,
+  // },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// 性能追蹤（可選）
 reportWebVitals();
