30f9e63656e4198ad69bf7da6d201e8f03589cd1
knyliu <131148428+knyliu@users.noreply.github.com>
2024-01-01 16:15:00 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require('path');
+
+
 const express = require('express');
 const app = express();
 
